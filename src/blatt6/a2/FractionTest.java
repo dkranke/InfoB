a26f4efbd256393953d7ba51f73a30d143088666
vlenz <vlenz@uos.de>
2018-06-05 02:15:33 +0200

    Test

--- src/blatt6/a2/FractionTest.java
+++ src/blatt6/a2/FractionTest.java
@@ -1,13 +1,51 @@
 package blatt6.a2;
 
-import util.Test;
-
 public class FractionTest {
+    private static String out;
 
     public static void main(String[] args) {
-        Test test = new Test();
+        Fraction f = new Fraction(2, 4);
+        out = "Fraction f = new Fraction(2,4)";
+        print();
+        out = "Fraction.parseFraction(\"1/2\") == f is ";
+        out += Fraction.parseFraction("1/2") == f;
+        print();
 
-        test.label("Konstruktor");
-        //TODO
+        f = new Fraction(0).add(new Fraction(1, 2));
+        out = "\nf = new Fraction(0).add(new Fraction(1,2));";
+        print();
+        out = "Fraction.parseFraction(\"1/2\") == f is ";
+        out += Fraction.parseFraction("1/2") == f;
+        print();
+
+        f = new Fraction(1).substract(new Fraction(1, 2));
+        out = "\nf = new Fraction(1).substract(new Fraction(1,2));";
+        print();
+        out = "Fraction.parseFraction(\"1/2\") == f is ";
+        out += Fraction.parseFraction("1/2") == f;
+        print();
+
+        f = new Fraction(1).multiply(new Fraction(1, 2));
+        out = "\nf = new Fraction(1).multiply(new Fraction(1,2));";
+        print();
+        out = "Fraction.parseFraction(\"1/2\") == f is ";
+        out += Fraction.parseFraction("1/2") == f;
+        print();
+
+        f = new Fraction(2).divide(new Fraction(4));
+        out = "\nf = new Fraction(2).divide(new Fraction(4));";
+        print();
+        out = "Fraction.parseFraction(\"1/2\") == f is ";
+        out += Fraction.parseFraction("1/2") == f;
+        print();
     }
+
+    private static void print() {
+        System.out.println(out);
+    }
+
+
+
+
+
 }
